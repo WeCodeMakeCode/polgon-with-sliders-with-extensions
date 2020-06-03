b129a023b440dec50964ac71699ce9f8154d53b5
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-06-02 20:31:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,28 @@
 controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
     right_or_left(1)
 })
-function right_or_left (inc: number) {
-    for (let value3 of sliders) {
-        if (value3.selected) {
-            value3.value += inc
-            if (value3.data == "sides") {
-                p.sides += inc
-            } else if (value3.data == "color") {
-                p.color += inc
-            } else {
-                p.angle += inc
-            }
-        }
-    }
-}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (game.runtime() - time > time_inc) {
-        b_found = false
-        ndx = 0
-        while (!(b_found) && ndx < sliders.length) {
-            if (sliders[ndx].selected) {
-                sliders[ndx].selected = false
-                sliders[(ndx + 1) % sliders.length].selected = true
-                b_found = true
-            }
-            ndx += 1
+        active_slider.selected = false
+        if (active_slider == slider_sides) {
+            active_slider = slider_color
+        } else if (active_slider == slider_color) {
+            active_slider = slider_starting_angle
+        } else {
+            active_slider = slider_sides
         }
-        if (!(b_found)) {
-            sliders[0].selected = true
-        }
+        active_slider.selected = true
         time = game.runtime()
     }
 })
 function set_slider_positions (width: number) {
     slider_between = (160 - 3 * width) / 4
     this_left = slider_between
-    for (let value of sliders) {
-        value.left = this_left
-        this_left = this_left + (width + slider_between)
-    }
+    slider_sides.left = this_left
+    this_left = this_left + (width + slider_between)
+    slider_color.left = this_left
+    this_left = this_left + (width + slider_between)
+    slider_starting_angle.left = this_left
 }
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     if (game.runtime() - time > time_inc) {
@@ -49,18 +32,26 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (game.runtime() - time > time_inc) {
-        for (let value2 of sliders) {
-            if (value2.selected) {
-                if (value2.thumb_text.isEmpty()) {
-                    value2.thumb_text = value2.data
-                } else {
-                    value2.thumb_text = ""
-                }
-            }
+        if (active_slider.thumb_text.isEmpty()) {
+            active_slider.thumb_text = active_slider.data
+        } else {
+            active_slider.thumb_text = ""
         }
-        time = game.runtime()
     }
+    time = game.runtime()
 })
+function right_or_left (inc: number) {
+    if (active_slider == slider_sides) {
+        active_slider.value += inc
+        p.sides += inc
+    } else if (active_slider == slider_color) {
+        active_slider.value += inc
+        p.color += inc
+    } else {
+        active_slider.value += inc * (360 / p.sides)
+        p.angle += inc * (360 / p.sides)
+    }
+}
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     if (game.runtime() - time > time_inc) {
         right_or_left(1)
@@ -72,22 +63,24 @@ controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
 })
 let this_left = 0
 let slider_between = 0
-let b_found = false
+let active_slider: Slider = null
+let slider_color: Slider = null
+let slider_starting_angle: Slider = null
+let slider_sides: Slider = null
 let p: Polygon = null
 let time_inc = 0
 let time = 0
 time = game.runtime()
 time_inc = 2000
-let sliders: Slider[] = []
-let ndx = 0
 p = polygon.createPolygon(3, 30, 6, 0)
 let slider_width = 40
-let slider_sides = slider.create(3, 3, 30, slider_width, 6)
-let slider_starting_angle = slider.create(0, 0, 360, slider_width, 6)
-let slider_color = slider.create(6, 1, 15, slider_width, 6)
-sliders = [slider_sides, slider_starting_angle, slider_color]
+slider_sides = slider.create(3, 3, 30, slider_width, 6)
+slider_starting_angle = slider.create(0, 0, 360, slider_width, 6)
+slider_starting_angle.increment = 30
+slider_color = slider.create(6, 1, 15, slider_width, 6)
 slider_sides.data = "sides"
 slider_starting_angle.data = "starting angle"
 slider_color.data = "color"
 slider_sides.selected = true
 set_slider_positions(slider_width)
+active_slider = slider_sides
